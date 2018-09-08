393a43dff69cb5b162c6c83931a9651abf73c19e
Nilesh <nileshtupe31@gmail.com>
2018-09-08 11:40:12 +0530

    Initial Commit

---
+++ VideoChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoChatApp
+//
+//  Created by Nilesh on 08/09/18.
+//  Copyright © 2018 Nilesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
